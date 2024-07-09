4cb925fd406b005c23906c17fd04e98ef196aabc
tutiolmedo <agustinaaolme123@gmail.com>
2024-07-09 18:52:17 -0300

    agregando js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Ingrese nombre");
+let apellido = prompt("Ingrese apellido");
+let nombreCompleto = nombre + " " + apellido;
+let mensajeFinal = "Bienvendi@, " + nombreCompleto
+alert("Hola, " + nombreCompleto)
